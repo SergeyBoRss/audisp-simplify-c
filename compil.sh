be1e3581083ee5300951adb87f2a8db05d024392
Sergey Borisov <66353172+SergeyBoRss@users.noreply.github.com>
2025-01-25 01:19:35 +0300

    Update compil.sh

--- compil.sh
+++ compil.sh
@@ -5,23 +5,12 @@ rm -f audisp-simplify-c-str-function.o
 rm -f audisp-simplify-c-filter.o
 rm -f audisp-simplify-c
 rm -f /bin/audisp-simplify-c
-if [ `uname -p` == 'x86_64' ]
-then
-    echo '64bit'
-    gcc -c audisp-simplify-c-filter.cpp
-    gcc -c audisp-simplify-c-str-function.cpp
-    gcc -c audisp-simplify-c-thread.cpp
-    gcc -c audisp-simplify-c.cpp
-    gcc -o audisp-simplify-c audisp-simplify-c.o audisp-simplify-c-thread.o audisp-simplify-c-str-function.o audisp-simplify-c-filter.o -lstdc++ -lm -lpthread
 
-    #gcc audisp-simplify-c.cpp -o audisp-simplify-c -lstdc++ -lm -lpthread
-else
-    gcc -c audisp-simplify-c-filter.cpp
-    gcc -c audisp-simplify-c-str-function.cpp
-    gcc -c audisp-simplify-c-thread.cpp
-    gcc -c audisp-simplify-c.cpp
-    gcc -o audisp-simplify-c audisp-simplify-c.o audisp-simplify-c-thread.o audisp-simplify-c-str-function.o audisp-simplify-c-filter.o -lstdc++ -lm -lpthread
-fi
+gcc -c audisp-simplify-c-filter.cpp
+gcc -c audisp-simplify-c-str-function.cpp
+gcc -c audisp-simplify-c-thread.cpp
+gcc -c audisp-simplify-c-d.cpp
+gcc -o audisp-simplify-c audisp-simplify-c.o audisp-simplify-c-thread.o audisp-simplify-c-str-function.o audisp-simplify-c-filter.o -lstdc++ -lm -lpthread -lz
 
 chmod 755 audisp-simplify-c
 cp -pv audisp-simplify-c /bin/
