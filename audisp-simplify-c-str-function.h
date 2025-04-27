ebb8d54893741fafa322cf75337002294e92da83
Sergey Borisov <66353172+SergeyBoRss@users.noreply.github.com>
2025-04-27 22:07:34 +0300

    Update audisp-simplify-c-str-function.h

--- audisp-simplify-c-str-function.h
+++ audisp-simplify-c-str-function.h
@@ -12,23 +12,30 @@
 extern bool DEBUG;
 extern bool DEBUG_DISPLAY;
 extern int  DEBUG_LEVEL;
+extern bool DEBUG_PROFILE;
+extern double DISPLAY_PROFILE_OVER;
+extern double DISPLAY_PROFILE_OVER_F_parsing_line;
+extern double DISPLAY_PROFILE_OVER_parsing_string_to_auditid;
+
 extern char *msg;
 extern mutex MTX_deblog;
+extern mutex MTX_save_debug_run;
 extern atomic_int ATOM_i_msg;
 extern atomic_bool ATOM_save_debug_run;
+extern atomic_bool ATOM_debug_simply;
 //extern FILE *f_debug;
 extern const char *deblogfile;
 
 extern atomic_int ATOM_prev_delta_strpos_istart;
 extern atomic_bool ATOM_enable_scan_extend_UID;
 
+void save_err(char *msg);
 void save_deblog();
 void deblog(char *msg);
 void debbuf(int istart,int iend,char *buf);
 void printbuf(char *buf);
 void print_ALL_audit();
-void print_cur_audit(s_audit cur_audit);
-void print_audit(s_audit *f_array,int auditid);
+
 void clear_buf(int istart,int iend,char *buf);
 int strpos_istart(char *bufstr,int start_i,int end_i,char *searchstr);
 int copy_val_istart(char *val, char *bufstr, int start_i, int end_i, char *filter, char stop_char,int max_char,int prev_delta_pos_find_val);
@@ -40,9 +47,11 @@ int auditid_to_id(s_audit *f_array, int array_count, unsigned int test_auditid);
 int uidtouser(char *login,uid_t uid);
 int gidtogroup(char *grp,gid_t gid);
 bool xlate_saddr(s_audit *c_audit, char *saddr);
-//int filtering(s_audit *f_array,int array_count,int n_thread);
+
 int cur_audit_to_array(s_audit *f_array,int array_count,s_audit cur_audit,int n_thread);
 int count_array_audit(int start_calc /*0*/);
 int F_parsing_string_to_auditid(char *buf, int start_i, int end_i, s_audit *f_array,int n_thread);
+void clear_array_audit_id(s_audit *f_array,int id);
+bool find_in_text(char * str, char *search_str, char separate);
 
 #endif // __AUDISP_SIMPLIFY_C_STR_FUNCTION_H__
