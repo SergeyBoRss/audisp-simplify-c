c2ee3b3f7bf32253b0d5cb8c01bfd8c4ce22d2e5
Sergey Borisov <66353172+SergeyBoRss@users.noreply.github.com>
2025-04-27 22:03:05 +0300

    Update audisp-simplify-c.cpp

--- audisp-simplify-c.cpp
+++ audisp-simplify-c.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,18 +11,25 @@
 
 using namespace std;
 
-bool DEBUG=false;
-bool DEBUG_DISPLAY=false;
-int  DEBUG_LEVEL=0;
+bool   DEBUG=false;
+bool   DEBUG_DISPLAY=false;
+int    DEBUG_LEVEL=0;
+bool   DEBUG_PROFILE=false;
+double DISPLAY_PROFILE_OVER=0.000100;
+double DISPLAY_PROFILE_OVER_F_parsing_line=0.000300;
+double DISPLAY_PROFILE_OVER_parsing_string_to_auditid=0.000900;
+bool   FILTER=ON;
 
 const char *ignorefile="/etc/audit/simplify.ignores";
 const char *logfile="/var/log/audisp-simplify-c";
+const char *errfile="/var/log/audisp-simplify-c.error";
 const char *storefile="/var/log/audisp-simplify-c.store";
 const char *compressfile="/var/log/audisp-simplify-c";
 const char *uncompressfile="/var/log/audisp-simplify-c.uncompress";
-const char *deblogfile="/var/log/audisp-simplify-c.log";
+const char *deblogfile="/var/log/audisp-simplify-c.debug";
 const char *statfile="/var/log/audisp-simplify-c.stat";
-const char *adminfile="/var/lib/audisp-simplify-c";
+const char *adminfile="/var/lib/audisp-simplify-c.cmd";
+const char *profilingfile="/var/lib/audisp-simplify-c.profiling";
 
 int main(int argc,char *argv[])
 {
@@ -44,8 +50,8 @@ int main(int argc,char *argv[])
       DEBUG_DISPLAY=true;
     }
   }
-  //if (DEBUG==true)
-    msg=(char *)malloc(sizeof(char) * SIZE_MSG);
+
+  msg=(char *)malloc(sizeof(char) * SIZE_MSG);
   deblog((char *)"=======================\n================== start =========");
   if ((DEBUG==true) || (DEBUG_DISPLAY==true))
     snprintf(msg_main,255,"pid=%d ppid=%d",pid,ppid);
@@ -61,38 +67,36 @@ int main(int argc,char *argv[])
   memset(array_group,0,sizeof(s_group) * COUNT_CACHE_GROUP);
   array_STAT_UID=(s_STAT_UID *)malloc(sizeof(s_STAT_UID) * COUNT_STAT_UID);
   memset(array_STAT_UID,0,sizeof(s_STAT_UID) * COUNT_STAT_UID);
-  //skipped_queue=(s_skipped_queue *)malloc(sizeof(s_skipped_queue) * SIZE_SKIPPED_QUEUE);
-  //memset(skipped_queue,0,sizeof(s_skipped_queue) * SIZE_SKIPPED_QUEUE);
+
+
   // === allocate memory ===
-	// === ignore file ===
-	//print_hash_audit_reserved_key();
 
   size_audit_reserved_key=init_available_hash_ignore_key();
-	read_ignorefile_to_buf(read_buf,size_buf);
-	if (ATOM_count_ignore_key.load()>0)
-	{
-		array_ignore=(s_ignore *)malloc(sizeof(s_ignore) * ATOM_count_ignore_key);
-	  memset(array_ignore,0,sizeof(s_ignore) * ATOM_count_ignore_key);
-		buf_to_ignore_array(read_buf,SIZE_BUF);
-    deblog("-------- ignore -----");
-    if (DEBUG_LEVEL>2)
-      printignore();
-	}
-
-	// === ignore file ===
-
+  // ================== ignore file =========================
+  if (FILTER==ON)
+  {
+  	read_ignorefile_to_buf(read_buf,size_buf);
+  	if (ATOM_count_ignore_key.load()>0)
+  	{
+  		array_ignore=(s_ignore *)malloc(sizeof(s_ignore) * ATOM_count_ignore_key);
+  	  memset(array_ignore,0,sizeof(s_ignore) * ATOM_count_ignore_key);
+  		buf_to_ignore_array(read_buf,SIZE_BUF);
+      deblog((char *)"-------- ignore -----");
+      if (DEBUG_LEVEL>2)
+        printignore();
+  	}
+  }
+  // ================== ignore file =========================
 
   sem_init(&SEM_relocate_buf,0,0);
   sem_init(&SEM_relocate_audit,0,0);
   sem_init(&SEM_line_read,0,0);
   sem_init(&SEM_run_parsing_line,0,0);
-  sem_init(&SEM_array_audit_lock,0,3);
   sem_init(&SEM_save,0,0);
 
-
-  pthread_create(&T_coordinator,           NULL, F_coordinator,          (void*)read_buf);
   pthread_create(&T_read_STDIN,            NULL, F_read_STDIN,           (void*)read_buf);
   pthread_create(&T_relocate_buf_to_start, NULL, F_relocate_buf_to_start,(void*)read_buf);
+  pthread_create(&T_coordinator,           NULL, F_coordinator,          (void*)read_buf);
   pthread_create(&T_parsing_buf,           NULL, F_parsing_buf,          (void*)read_buf);
   for (int i=0; i<COUNT_PARALLEL_PARSING; i++)
   {
@@ -103,56 +107,32 @@ int main(int argc,char *argv[])
   pthread_create(&T_relocate_audit,        NULL, F_relocate_audit,       (void*)array_audit);
   pthread_create(&T_stat,                  NULL, F_stat,                 NULL);
 
-
   pthread_join(T_read_STDIN,NULL);
-  deblog("end T_read_STDIN");
+  deblog((char *)"end T_read_STDIN");
+
   pthread_join(T_relocate_buf_to_start,NULL);
-  deblog("end T_relocate_buf_to_start");
+  deblog((char *)"end T_relocate_buf_to_start");
   pthread_join(T_parsing_buf,NULL);
-  deblog("end T_parsing_buf");
+  deblog((char *)"end T_parsing_buf");
 
   for (int i=0; i<COUNT_PARALLEL_PARSING; i++)
   {
     pthread_join(T_parsing_line[i],NULL);
-    deblog("end T_parsing_line[i]");
+    deblog((char *)"end T_parsing_line[i]");
   }
-  deblog("end T_parsing_line");
+  deblog((char *)"end T_parsing_line");
   pthread_join(T_relocate_audit,NULL);
-  deblog("end T_relocate_audit");
+  deblog((char *)"end T_relocate_audit");
   pthread_join(T_save_file,NULL);
-  deblog("end T_save_file");
-  pthread_join(T_stat,NULL);
-  deblog("end T_stat");
+  deblog((char *)"end T_save_file");
   pthread_join(T_coordinator,NULL);
-  deblog("end T_coordinator");
-  //free(skipped_queue);
+  deblog((char *)"end T_coordinator");
+  pthread_join(T_stat,NULL);
+  deblog((char *)"end T_stat");
 
   if ((DEBUG_DISPLAY==true) && (DEBUG_LEVEL>2))
     printf("start free\n");
-  //free(read_buf);
-  if ((DEBUG_DISPLAY==true) && (DEBUG_LEVEL>2))
-    printf("free read_buf\n");
-  free(array_pass);
-  if ((DEBUG_DISPLAY==true) && (DEBUG_LEVEL>2))
-    printf("free array_pass\n");
-  free(array_group);
-  if ((DEBUG_DISPLAY==true) && (DEBUG_LEVEL>2))
-    printf("free array_group\n");
-  free(array_STAT_UID);
-  free(array_audit);
-  if ((DEBUG_DISPLAY==true) && (DEBUG_LEVEL>2))
-    printf("free array_audit\n");
-  free(available_hash_ignore_key);
-  if ((DEBUG_DISPLAY==true) && (DEBUG_LEVEL>2))
-    printf("free available_hash_ignore_key\n");
 
-  if ((DEBUG==true) && (ATOM_save_debug_run.load()==true))
-  {
-    printf("wait save debug\n");
-    sleep(1);
-  }
-  free(msg);
-  if ((DEBUG_DISPLAY==true) && (DEBUG_LEVEL>2))
-    printf("free msg\n");
+  save_deblog();
   return 0;
 }
